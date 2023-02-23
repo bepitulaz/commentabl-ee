cbbd9d32d0fb95d1153ba814388ef92b204586bf
Asep Bagja Priandana <bepitulaz@gmail.com>
2023-02-23 18:36:49 +0200

    Completing the basic form submission

--- web-client/src/CommentForm.js
+++ web-client/src/CommentForm.js
@@ -1,3 +1,4 @@
+import { useEffect } from 'react'
 import { useForm } from 'react-hook-form'
 import {
   Box,
@@ -17,41 +18,51 @@ function CommentForm() {
   const {
     register,
     handleSubmit,
-    watch,
-    formState: { errors },
+    formState: { errors, isSubmitSuccessful },
+    reset,
   } = useForm()
-  console.log(errors);
+
   const onSubmit = (data) => {
     console.log(data)
   }
 
+  useEffect(() => {
+    if (isSubmitSuccessful) {
+      reset()
+    }
+  }, [isSubmitSuccessful, reset])
+
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
-      <FormControl>
-        <Box py={1}>
-          <FormLabel>Your name</FormLabel>
-          <Input type="text" {...register('fullname', { required: true })} />
-          <FormErrorMessage></FormErrorMessage>
-        </Box>
-        <Box py={1}>
-          <FormLabel>Your email</FormLabel>
-          <Input type="email" {...register('email')} />
-          <FormHelperText>{EMAIL_TEXT}</FormHelperText>
-        </Box>
-        <Box py={1}>
-          <FormLabel>Your comment</FormLabel>
-          <Textarea
-            type="text"
-            placeholder="What's on your mind?"
-            {...register('comment', { required: true })}
-          />
-        </Box>
-        <Box py={2}>
-          <Button type="submit" colorScheme="red">
-            Submit my comment
-          </Button>
-        </Box>
+      <FormControl py={1} isInvalid={'fullname' in errors}>
+        <FormLabel>Your name</FormLabel>
+        <Input
+          type="text"
+          {...register('fullname', { required: 'Your name is required' })}
+        />
+        <FormErrorMessage>{errors?.fullname?.message}</FormErrorMessage>
       </FormControl>
+      <FormControl py={1}>
+        <FormLabel>Your email</FormLabel>
+        <Input type="email" {...register('email')} />
+        <FormHelperText>{EMAIL_TEXT}</FormHelperText>
+      </FormControl>
+      <FormControl py={1} isInvalid={'comment' in errors}>
+        <FormLabel>Your comment</FormLabel>
+        <Textarea
+          type="text"
+          placeholder="What's on your mind?"
+          {...register('comment', {
+            required: "Don't forget to write your comment",
+          })}
+        />
+        <FormErrorMessage>{errors?.comment?.message}</FormErrorMessage>
+      </FormControl>
+      <Box py={2}>
+        <Button type="submit" colorScheme="teal">
+          Submit my comment
+        </Button>
+      </Box>
     </form>
   )
 }
